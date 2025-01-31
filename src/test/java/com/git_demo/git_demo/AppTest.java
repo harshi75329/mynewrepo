048cc71ceea463c18b064fe2c2ecaf4883110fd8
harshi75329 <harshithaadusumalli7@gmail.com>
2025-01-31 15:28:06 +0530

    first commit

---
+++ src/test/java/com/git_demo/git_demo/AppTest.java
@@ -0,0 +1,19 @@
+package com.git_demo.git_demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
