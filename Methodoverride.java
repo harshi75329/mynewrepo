4d1fc2d48df86d2e126344fce82e685053f6a769
harshi75329 <harshithaadusumalli7@gmail.com>
2025-02-03 12:59:42 +0530

    A2

---
+++ Methodoverride.java
@@ -0,0 +1,37 @@
+package Javaoops;
+
+
+class Phone{
+	public void cost() {
+		System.out.println("below 10,000");
+	}
+}
+class Iqoo extends Phone{
+	public void cost() {
+		System.out.println("Cost of Iqoo : "+ "50,000");
+	}
+}
+class Samsung extends Phone{
+	public void cost() {
+		System.out.println("Cost of Samsung : "+ "90,000");
+	}
+}
+class Iphone extends Phone{
+	public void cost() {
+		System.out.println("Cost of IPhone : "+ "1,30,000");
+	}
+}
+public class Methodoverride {
+	public static void main(String args[]) {
+		Phone NormalPhone = new Phone();
+		Iqoo Iqoo = new Iqoo();
+		Samsung Samsung = new Samsung();
+		Iphone Iphone = new Iphone();
+		NormalPhone.cost();
+		Iqoo.cost();
+		Samsung.cost();
+		Iphone.cost();
+	}
+
+}
+
